8dbd96184755091b1f064869c29df33fcfbf4b46
Anish Gharti <anishgharti10@gmail.com>
2024-03-26 21:58:23 +0545

    user added

--- snippets/models.py
+++ snippets/models.py
@@ -23,4 +23,6 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+
     created_at = models.DateTimeField(auto_now_add=True)
